50aa9c518dfaefc2ac62b7f99d312214a9c69d43
Yaroslav <yaryrslv@gmail.com>
2021-03-31 23:53:17 +0300

    Add AdService

---
+++ Services/Implementation/IAdService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BulletinBoardAPI.Models.Realizations;
+
+namespace BulletinBoardAPI.Services.Implementation
+{
+    public interface IAdService
+    {
+        Task<IEnumerable<Ad>> GetAllAsync();
+        Task<Ad> GetAsync(Guid id);
+        void CreateAsync(Ad item);
+        void UpdateAsync(Ad item);
+        Task<Ad> DeleteAsync(Guid id);
+    }
+}
---
+++ Services/Realization/AdService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulletinBoardAPI.EF;
+using BulletinBoardAPI.Models.Realizations;
+using BulletinBoardAPI.Services.Implementation;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulletinBoardAPI.Services.Realization
+{
+    public class AdService : IAdService
+    {
+        private BulletionBoardContext _context;
+        public AdService(BulletionBoardContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Ad>> GetAllAsync()
+        {
+            return await _context.AdItems.Where(i => i != null).ToListAsync();
+        }
+        public async Task<Ad> GetAsync(Guid id)
+        {
+            return await _context.AdItems.FindAsync(id);
+        }
+        
+        public async void CreateAsync(Ad item)
+        {
+            await _context.AdItems.AddAsync(item);
+            await _context.SaveChangesAsync();
+        }
+        public async void UpdateAsync(Ad item)
+        {
+            _ = await GetAsync(item.Id);
+            Ad currentItem = item;
+            _context.AdItems.Update(currentItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Ad> DeleteAsync(Guid id)
+        {
+            Ad adItem = await GetAsync(id);
+
+            if (adItem != null)
+            {
+                _context.AdItems.Remove(adItem);
+                await _context.SaveChangesAsync();
+            }
+
+            return await _context.AdItems.FindAsync(adItem.Id);
+        }
+    }
+}
